b6efc0441f86125694f1e250a4fb4928c683a89c
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2023-11-27 16:45:46 +0000

    use aria2 for qget

--- aliases.zsh
+++ aliases.zsh
@@ -4,5 +4,5 @@ alias ls='eza -G'
 alias ll='eza -lG'
 alias lsa='eza -lahG'
 
-alias qget='NQDIR=~/Downloads nq wget'
+alias qget='NQDIR=~/Downloads nq aria2c -d ~/Downloads'
 alias qwait='NQDIR=~/Downloads fq -q'
