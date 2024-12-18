ca74d0af6a5da9e6efc753191ddd2e2be2d1d15d
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2024-12-18 01:02:06 +0000

    add pincode alias to generate random 6-digit codes

--- aliases.zsh
+++ aliases.zsh
@@ -14,3 +14,5 @@ alias qwait='NQDIR=~/Downloads fq -q'
 
 # Honeyman’s “idiom” command:
 alias honey='sort | uniq -c | sort -rn'
+
+alias pincode='shuf --random-source=/dev/urandom -i 0-9 -r -n 6 | paste -sd ""'
